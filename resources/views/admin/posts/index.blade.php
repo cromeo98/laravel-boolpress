94aa0525d939c86f783ce6511b3aa70988383bf5
cromeo98 <romeo.christian98@gmail.com>
2021-09-19 13:18:29 +0200

    created side bar

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,6 +2,34 @@
 
 @section('content')
 
-    
+    <div class="container">
+        <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">ID Post</th>
+                <th scope="col">Title</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <th scope="row">{{$post->id}}</th>
+                        <td>{{$post->title}}</td>
+                        <td>
+                            <a href="" class="btn btn-outline-success">Show</a>
+                            <a href="" class="btn btn-outline-warning">Edit</a>
+                            <form action="" class="d-inline-block" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="delete" class="btn btn-outline-danger">
+                            </form>
+                        </td>
+                    </tr>                    
+                @endforeach
+
+            </tbody>
+          </table>
+    </div>
     
 @endsection
