c5ca8116d94f392c10621add2e25a04be29ae2b4
cromeo98 <romeo.christian98@gmail.com>
2021-09-19 13:51:02 +0200

    add create

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use App\Post;
 
 class PostController extends Controller
@@ -28,7 +29,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.posts.create');
     }
 
     /**
@@ -39,7 +40,16 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newPost = new Post;
+
+        $newPost->fill($data);
+        $newPost->slug = Str::slug($data['title']);
+
+        $newPost->save();
+        
+        return redirect()->route('admin.posts.show', $newPost->id);
     }
 
     /**
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,7 +7,7 @@
             <h5 class="card-title">{{$post->title}}</h5>
             <h6 class="card-subtitle mb-2 text-muted">{{$post->slug}}</h6>
             <p class="card-text">{{$post->content}}</p>
-            <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna indietro</a>
+            <a href="{{route('admin.posts.index')}}" class="btn btn-primary">Torna alla lista dei post</a>
         </div>
     </div>
 @endsection
